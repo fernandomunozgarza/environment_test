1354b7a4fc51dc7e32404e4de07491cae7524375
fernandomunozgarza <fer.munoz2012@gmail.com>
2026-09-13 09:51:56 +0000

    Fixed syntax error in books_spec.rb

--- spec/models/books_spec.rb
+++ spec/models/books_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
     describe "expected titles" do
         it "has test1 in test environment" do
-            book = Book.find_by(title: "test1")
-            expect(book).to exist
+            expect(Book.where(title: "test1")).to exist
         end
     end
 end
