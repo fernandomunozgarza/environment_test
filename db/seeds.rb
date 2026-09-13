24bb43e14977f991d9a17691b3364d10c9cedd26
fernandomunozgarza <fer.munoz2012@gmail.com>
2026-09-13 08:10:49 +0000

    Added seeds for dev and prod.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
-Book.create!([
-  {title: "Excited for Fall 2026"},
-  {title: "because the internet"},
-  {title: "The Catcher in the Rye"},
-  {title: "To Kill a Mockingbird"},
-  {title: "1984"},
-  {title: "The Great Gatsby"},
-])
+if Rails.env.development?
+    Book.create!([
+    {title: "Excited for Fall 2026"},
+    {title: "because the internet"},
+    {title: "The Catcher in the Rye"},
+    {title: "To Kill a Mockingbird"},
+    {title: "1984"},
+    {title: "The Great Gatsby"},
+    ])
+elsif Rails.env.production?
+    Book.create!([
+    {title: "a"},
+    {title: "b"},
+    {title: "c"},
+    {title: "d"},
+    {title: "e"},
+    {title: "f"},
+    ])
+end
